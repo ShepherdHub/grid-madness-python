be6cfbd181694508e16a1e985f6f0b2aee0f0d2b
Alex Rosenthal <alrosent1@gmail.com>
2018-07-18 19:03:21 -0400

    fix: Adjust weights of diagonals

--- graph.py
+++ graph.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 
 class Graph:
         
@@ -23,45 +24,70 @@ class Graph:
         print self.grid
     #*******END TESTING: TERMINAL DISPLAY*******
 
-    def adjacency(self,i,j, distance = 1):
+    def adjacency(self,i,j  ):
         #Better way with something similar to networkx?
 
-        adj = [(i,j+1),(i+1,j+1),(i+1,j),(i+1,j-1),(i,j-1),(i-1,j-1),(i-1,j),(i-1,j+1)]
+        #adj = [(i,j+1),(i+1,j+1),(i+1,j),(i+1,j-1),(i,j-1),(i-1,j-1),(i-1,j),(i-1,j+1)]
+        adj = [(i,j+1),(i+1,j),(i,j-1),(i-1,j)]
+        dia = [(i+1,j+1),(i+1,j-1),(i-1,j-1),(i-1,j+1)]
         
         if i == 0:
             #Corner cases
             if j == 0:
-                adj = adj[0:3]
+                #adj = adj[0:3]
+                adj = adj[0:2]
+                dia = [dia[0]]
             elif j == self.size-1:
-                adj = adj[2:5]
+                #adj = adj[2:5]
+                adj = adj[1:3]
+                dia = [dia[1]]
             #Side
             else:
-                adj = adj[0:5]
+                #adj = adj[0:5]
+                adj = adj[0:3]
+                dia = dia[0:2]
 
         elif i == self.size-1:
             #Corner cases
             if j == 0:
                 #Better way?
-                adj = adj[6:8]+[adj[0]]
+                #adj = adj[6:8]+[adj[0]]
+                adj = [adj[3]]+[adj[0]]
+                dia = [dia[3]]
+
             elif j == self.size-1:
-                adj = adj[4:7]
+                #adj = adj[4:7]
+                adj = adj[2:4]
+                dia = [dia[2]]
+
             #Side
             else:
-                adj = [adj[0]]+adj[4:8]
+                #adj = [adj[0]]+adj[4:8]
+                adj = [adj[0]]+adj[2:4]
+                dia = dia[2:4]
         
         #Sides
         elif j == 0:
-            adj = adj[6:8]+adj[0:3]
+            #adj = adj[6:8]+adj[0:3]
+            adj = [adj[3]]+adj[0:2]
+            dia = [dia[3]]+[dia[0]]
+
         elif j == self.size-1:
-            adj = adj[2:7]
+            #adj = adj[2:7]
+            adj = adj[1:4]
+            dia = dia[1:3]
 
         #Remove adjacent obstacles
         for obs in self.obstacles:
             if obs in adj:
                 adj.remove(obs)
+            if obs in dia:
+                dia.remove(obs)
         
         #Convert to dict
-        adj = {v:distance for v in adj}
+        adj = {v:1 for v in adj}
+        for d in dia:
+            adj[d] = math.sqrt(2)
 
         return adj
 
