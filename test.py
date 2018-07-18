8a1de0822a3e82ccdba4c52407e9e88f81cf211e
Alex Rosenthal <alrosent1@gmail.com>
2018-07-18 17:03:40 -0400

    feat: Test dijkstra.py, display path and obstacles to terminal

--- test.py
+++ test.py
@@ -1,4 +1,10 @@
 from graph import Graph
-test_grid = {'size':10, 'obstacles':[(3,3),(3,4),(3,5),(3,6),(4,6),(5,6),(6,6)], 'start':(0,0),'end':(9,9)}
+from dijkstra import dijkstra
+test_grid = {'size':10, 'obstacles':[(3,0),(3,1),(3,2),(3,3),(3,4),(3,5),(3,6),(4,6),(5,6),(6,6)], 'start':(0,0),'end':(9,9)}
 g = Graph(test_grid)
-g.terminal_display()
+_,_,path = dijkstra(test_grid)
+p = g.make_obs()
+for v in path:
+    p[v] = 111
+    print v
+print p
